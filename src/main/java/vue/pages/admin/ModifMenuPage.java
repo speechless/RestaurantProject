8bc0dee5a854282f6b4d2065244dcc5361b6175b
t-vanderb <thom4.vdb@gmail.com>
2025-01-14 22:15:01 +0100

    Modification menu

--- src/main/java/vue/pages/admin/ModifMenuPage.java
+++ src/main/java/vue/pages/admin/ModifMenuPage.java
@@ -97,28 +97,49 @@ public class ModifMenuPage implements PageContent {
 
 
         JButton boutonNouveauMenu = ButtonTemplates.setupClassicButton("Créer un menu",
-                () -> PageManager.getInstance().showPage(new ModifMenu(0)));
+                () -> PageManager.getInstance().showPage(new ModifMenu()));
         boutonNouveauMenu.setFont(new Font("Arial", Font.PLAIN, 12));
         boutonNouveauMenu.setMargin(new Insets(0, 0, 0, 0)); // Supprime les marges internes
         boutonNouveauMenu.setPreferredSize(new Dimension(150, 30));
-        gbc.gridx = 0; // Colonne
-        gbc.gridy = 1; // Ligne
-        gbc.gridwidth = 1; // Étend sur une colonne
-        gbc.weightx = 1.0; // S'étend horizontalement
-        gbc.weighty = 1.0; // Prend tout l'espace vertical
-        mainPanel.add(boutonNouveauMenu, gbc);
 
-        JButton boutonNouvelItem = ButtonTemplates.setupClassicButton("Créer un produit",
+        JButton boutonNouvelItem;
+        boutonNouvelItem = ButtonTemplates.setupClassicButton("Créer un produit",
                 () -> PageManager.getInstance().showPage(new ModifItem()));
         boutonNouvelItem.setFont(new Font("Arial", Font.PLAIN, 12));
         boutonNouvelItem.setMargin(new Insets(0, 0, 0, 0)); // Supprime les marges internes
         boutonNouvelItem.setPreferredSize(new Dimension(150, 30));
-        gbc.gridx = 1; // Colonne
-        gbc.gridy = 1; // Ligne
-        gbc.gridwidth = 1; // Étend sur deux colonnes
-        gbc.weightx = 1.0; // S'étend horizontalement
-        gbc.weighty = 1.0; // Prend tout l'espace vertical
-        mainPanel.add(boutonNouvelItem, gbc);
+
+        if (type == TypeAffichage.BOTH) {
+            gbc.gridx = 0; // Colonne
+            gbc.gridy = 1; // Ligne
+            gbc.gridwidth = 1; // Étend sur une colonne
+            gbc.weightx = 1.0; // S'étend horizontalement
+            gbc.weighty = 1.0; // Prend tout l'espace vertical
+            mainPanel.add(boutonNouveauMenu, gbc);
+
+            gbc.gridx = 1; // Colonne
+            gbc.gridy = 1; // Ligne
+            gbc.gridwidth = 1; // Étend sur deux colonnes
+            gbc.weightx = 1.0; // S'étend horizontalement
+            gbc.weighty = 1.0; // Prend tout l'espace vertical
+            mainPanel.add(boutonNouvelItem, gbc);
+        }
+        else if (type == TypeAffichage.MENU) {
+            gbc.gridx = 0; // Colonne
+            gbc.gridy = 1; // Ligne
+            gbc.gridwidth = 1; // Étend sur une colonne
+            gbc.weightx = 1.0; // S'étend horizontalement
+            gbc.weighty = 1.0; // Prend tout l'espace vertical
+            mainPanel.add(boutonNouveauMenu, gbc);
+        }
+        else if (type == TypeAffichage.ITEM) {
+            gbc.gridx = 0; // Colonne
+            gbc.gridy = 1; // Ligne
+            gbc.gridwidth = 1; // Étend sur deux colonnes
+            gbc.weightx = 1.0; // S'étend horizontalement
+            gbc.weighty = 1.0; // Prend tout l'espace vertical
+            mainPanel.add(boutonNouvelItem, gbc);
+        }
 
 
         // Ajouter la section au mainPanel
---
+++ src/main/java/vue/utils/menu/MenuContentPanel.java
@@ -0,0 +1,87 @@
+package vue.utils.menu;
+
+import modele.Item;
+import modele.Menu;
+import requete.RequeteRestaurant;
+import vue.utils.CommandableRenderer;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class MenuContentPanel extends JPanel {
+
+    private Menu menu;
+    public JList<Item> menuContentList;
+    private DefaultListModel<Item> menuContentListModel;
+    private RequeteRestaurant rq = RequeteRestaurant.getInstance();
+
+    public MenuContentPanel(Menu menu) {
+        this.menu = menu;
+
+        this.setLayout(new GridBagLayout());
+        this.setBorder(new EmptyBorder(20, 20, 20, 10)); // Marges autour de la section
+
+        // Modèle de liste
+        DefaultListModel<Item> model = new DefaultListModel<>();
+        for (Item item : menu.getListeItems()) {
+            model.addElement(item);
+        }
+
+        this.menuContentListModel = model;
+
+        GridBagConstraints gbcOrderContentList = new GridBagConstraints();
+        gbcOrderContentList.gridx = 0;
+        gbcOrderContentList.gridy = 0;
+        gbcOrderContentList.weightx = 1.0;
+        gbcOrderContentList.weighty = 1.0;
+        gbcOrderContentList.fill = GridBagConstraints.BOTH; // La liste remplit tout l'espace disponible
+        gbcOrderContentList.insets = new Insets(5, 5, 5, 5);
+
+        // JList avec un renderer personnalisé
+        menuContentList = new JList<>(model);
+        menuContentList.setCellRenderer(new CommandableRenderer());
+
+
+        JScrollPane sp = new JScrollPane(this.menuContentList);
+
+        // Largeur à 80% via un panneau intermédiaire
+        JPanel listPanel = new JPanel(new BorderLayout());
+        listPanel.add(sp, BorderLayout.CENTER);
+        listPanel.setPreferredSize(new Dimension((int) (this.getWidth() * 0.8), (int)(this.getHeight() * 0.8)));
+        this.add(sp, gbcOrderContentList);
+
+    }
+
+    public Menu removeProductFromMenu(Item elementSelectionne){
+        this.menu = rq.retirerProduitMenu(this.menu, elementSelectionne);
+
+        menuContentListModel.clear();
+        for (Item item : this.menu.getListeItems()) {
+            menuContentListModel.addElement(item);
+        }
+
+        return this.menu;
+    }
+
+    public Menu addProductToMenu(Item product) {
+        if (product != null) {
+            this.menu = rq.ajouterProduitMenu(this.menu, product);
+            actualiserAffichage();
+        }
+
+        return this.menu;
+    }
+
+    private void actualiserAffichage() {
+        Item itemSelectionnee = this.menuContentList.getSelectedValue();
+
+        menuContentListModel.clear();
+        for (Item item : this.menu.getListeItems()) {
+            menuContentListModel.addElement(item);
+            if (item.equals(itemSelectionnee)) {
+                this.menuContentList.setSelectedValue(itemSelectionnee, true);
+            }
+        }
+    }
+}
